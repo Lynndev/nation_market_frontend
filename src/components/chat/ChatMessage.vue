09eb8aa2f6b66a8bd9598c8733ab711e0c970778
nyihtutzaw <nyihtutzaw.2015@gmail.com>
2020-10-08 19:57:24 +0630

    Fix/admin chat (#5)
    * init setup
    
    * fix wrap up

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -1,111 +1,97 @@
 <template>
-  <v-responsive
-    v-if="newMessages.length > 0"
-    class="overflow-y-hidden fill-height"
-    height="670"
-  >
+<v-responsive v-if="organizedMessages.length > 0" class="overflow-y-hidden fill-height" height="670">
     <v-card class="d-flex flex-column fill-height">
-      <v-card-text
-        v-chat-scroll="{ smooth: true }"
-        class="flex-grow-1 overflow-y-auto"
-      >
-        <template v-for="(msg, i) in newMessages">
-          <div :key="i" :class="{ 'd-flex flex-row-reverse': msg.status == 2 }">
-            <v-chip
-              v-if="msg.body.type == 1"
-              :color="msg.status == 2 ? 'primary' : ''"
-              dark
-              style="height:auto;white-space: normal;"
-              class="pa-2 mb-2 message"
-            >
-              {{ msg.body.text }}
-            </v-chip>
-            <!-- if message is image  -->
-            <v-chip
-              v-else
-              :color="
+        <v-card-text v-chat-scroll="{ smooth: true }" class="flex-grow-1 overflow-y-auto">
+            <template v-for="(msg, i) in organizedMessages">
+                <div :key="i" :class="{ 'd-flex flex-row-reverse': msg.status == 2 }">
+                    <v-chip v-if="msg.body.type == 1" :color="msg.status == 2 ? 'primary' : ''" dark style="height:auto;white-space: normal;" class="pa-2 mb-2 message">{{ msg.body.text }}</v-chip>
+                    <!-- if message is image  -->
+                    <v-chip v-else :color="
                 msg.status == 2 || msg.body.type == 2 ? 'blue lighten-5' : ''
-              "
-              style="height:auto;white-space: normal;"
-              class="pa-2 mb-2 message"
-            >
-              <v-img alt="profile" :src="msg.body.image" />
-            </v-chip>
-            <sub class="ml-2" style="font-size: 0.5rem;">
-              {{ msg.created_at | formatDateTime }}
-            </sub>
-          </div>
-        </template>
-      </v-card-text>
-      <v-card-text class="flex-shrink-1">
-        <v-text-field
-          v-model="adminMessage"
-          label="type here..."
-          type="text"
-          no-details
-          outlined
-          dense
-          append-outer-icon="send"
-          append-icon="image"
-          @keyup.enter="sendMessageFromAdmin"
-          @click:append-outer="sendMessageFromAdmin"
-          @click:append="handleRefImage"
-          hide-details
-        />
-        <input
-          ref="image"
-          type="file"
-          style="display:none"
-          @change="sendImageFromAdmin"
-        />
-      </v-card-text>
+              " style="height:auto;white-space: normal;" class="pa-2 mb-2 message">
+                        <v-img alt="profile" :src="msg.body.image" />
+                    </v-chip>
+                    <sub class="ml-2" style="font-size: 0.5rem;">{{ msg.created_at | formatDateTime }}</sub>
+                </div>
+            </template>
+        </v-card-text>
+        <v-card-text class="flex-shrink-1">
+            <v-text-field v-model="adminMessage" label="type here..." type="text" no-details outlined dense append-outer-icon="send" append-icon="image" @keyup.enter="sendMessageFromAdmin" @click:append-outer="sendMessageFromAdmin" @click:append="handleRefImage" hide-details />
+            <input ref="image" type="file" style="display:none" @change="sendImageFromAdmin" />
+        </v-card-text>
     </v-card>
-  </v-responsive>
+</v-responsive>
 </template>
 
 <script>
+import {
+    mapState
+} from "vuex";
+
 export default {
-  props: {
-    newMessages: {
-      type: Array,
+    props: {
+        newMessages: {
+            type: Array
+        },
+        memberId: {
+            type: Number
+        }
     },
-    memberId: {
-      type: Number,
-    },
-  },
-  data() {
-    return {
-      adminMessage: "",
-    };
-  },
-  methods: {
-    handleRefImage() {
-      this.$refs.image.click();
-    },
-    sendImageFromAdmin(e) {
-      const files = e.target.files;
 
-      let formData = new FormData();
-      formData.append("member_id", this.memberId);
-      formData.append("body[type]", 2);
-      formData.append("image", files[0]);
+    data() {
+        return {
+            adminMessage: ""
+        };
+    },
+    methods: {
+        handleRefImage() {
+            this.$refs.image.click();
+        },
+        sendImageFromAdmin(e) {
+            const files = e.target.files;
 
-      this.$store.dispatch("Member/sendMessageFromAdmin", formData);
+            let formData = new FormData();
+            formData.append("member_id", this.$route.query.member_id);
+            formData.append("body[type]", 2);
+            formData.append("image", files[0]);
+
+            this.$store.dispatch("Member/sendMessageFromAdmin", formData).then(() => {
+                this.adminMessage = "";
+            });
+        },
+        sendMessageFromAdmin() {
+            let messageForm = {
+                member_id: this.$route.query.member_id,
+                body: {
+                    type: 1,
+                    text: this.adminMessage
+                }
+            };
+
+            this.$store
+                .dispatch("Member/sendMessageFromAdmin", messageForm)
+                .then(() => {
+                    this.adminMessage = "";
+                });
+        }
     },
-    sendMessageFromAdmin() {
-      let messageForm = {
-        member_id: this.memberId,
-        body: { type: 1, text: this.adminMessage },
-      };
-      console.log(messageForm);
-      this.$store.dispatch("Member/sendMessageFromAdmin", messageForm);
-    },
-  },
+    computed: {
+        organizedMessages() {
+            return this.newMessages.map(val => {
+                if (typeof val.body == "string") {
+                    let body = JSON.parse(val.body);
+                    val.body = body;
+                }
+                return val;
+            });
+        },
+        ...mapState("User", ["token"])
+    }
 };
 </script>
 
 <style scoped>
 .message {
-  max-width: 330px !important;
+    max-width: 330px !important;
 }
 </style>
--- src/components/includes/SnackBar.vue
+++ src/components/includes/SnackBar.vue
@@ -1,46 +1,44 @@
 <template>
-  <div>
-    <v-snackbar
-      v-model="notifications.status"
-      :right="true"
-      :top="true"
-      color="cyan darken-2"
-    >
-      {{ notifications.message }}
+<div>
+    <v-snackbar v-model="notifications.status" :right="true" :top="true" color="cyan darken-2">
+        {{ notifications.message }}
     </v-snackbar>
-  </div>
-  
+</div>
 </template>
 
 <script>
-import { mapState,mapActions } from 'vuex'
+import {
+    mapState,
+    mapActions
+} from 'vuex'
 
 export default {
-  data() {
-    return {
-      show:true,
-      timeout: null
-    }
-  },
-  mounted() {
-    this.timeout = setTimeout(() => this.remove(), 5000)
-  },
-  beforeDestroy() {
-    clearTimeout(this.timeout)
-  },
-  computed: mapState('Notification', ['notifications']),
-  methods: mapActions('Notification', ['remove'])
+    data() {
+        return {
+            show: true,
+            timeout: null
+        }
+    },
+    mounted() {
+        this.timeout = setTimeout(() => this.remove(), 5000)
+    },
+    beforeDestroy() {
+        clearTimeout(this.timeout)
+    },
+    computed: mapState('Notification', ['notifications']),
+    methods: mapActions('Notification', ['remove'])
 }
 </script>
 
 <style scoped>
 .notification-container {
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  padding-right: 40px;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    padding-right: 40px;
 }
+
 .notification-bar {
-  margin: 1em 0 1em;
+    margin: 1em 0 1em;
 }
 </style>
